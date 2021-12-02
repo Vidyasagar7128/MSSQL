c904163be59a0462c52ab0c8471cec8c0db991cd
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-12-03 00:38:17 +0530

    [Vidyasagar]: Added candidate_qualification Table

--- LMSdata.sql
+++ LMSdata.sql
@@ -83,3 +83,26 @@ CREATE TABLE candidate_bank_det(
   creator_stamp datetime DEFAULT NULL,
   creator_user int DEFAULT NULL,
 )
+-------------------------------------------------------------------------5) candidate_qualification---------------------------------------
+CREATE TABLE candidate_qualification(
+  id int iDENTITY PRIMARY KEY NOT NULL,
+  candidate_id int NOT NULL FOREIGN KEY REFERENCES fellowship_candidates(id),
+  diploma int DEFAULT 0,
+  degree_name varchar(10) NOT NULL,
+  is_degree_name_verified int DEFAULT 0,
+  employee_decipline varchar(100) NOT NULL,
+  is_employee_decipline_verified int DEFAULT 0,
+  passing_year int NOT NULL,
+  is_passing_year_verified int DEFAULT 0,
+  aggr_per float DEFAULT NULL,
+  final_year_per float DEFAULT NULL,
+  is_final_year_per_verified int DEFAULT 0,
+  training_institute varchar(20) NOT NULL,
+  is_training_institute_verified int DEFAULT 0,
+  training_duration_month int DEFAULT NULL,
+  is_training_duration_month_verified int DEFAULT 0,
+  other_training varchar(255) DEFAULT NULL,
+  is_other_training_verified int DEFAULT 0,
+  creator_stamp datetime DEFAULT NULL,
+  creator_user int DEFAULT NULL
+)
