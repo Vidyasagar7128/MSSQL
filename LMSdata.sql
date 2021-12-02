4f9452b959b208536045ef1a56e33b7e198c6d12
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-12-03 00:43:06 +0530

    [Vidyasagar]: Added candidates_education_det_check Table

--- LMSdata.sql
+++ LMSdata.sql
@@ -106,3 +106,14 @@ CREATE TABLE candidate_qualification(
   creator_stamp datetime DEFAULT NULL,
   creator_user int DEFAULT NULL
 )
+-------------------------------------------------------------------------6) candidates_education_det_check---------------------------------------
+CREATE TABLE candidates_education_det_check (
+  id int NOT NULL,
+  candidate_id  int NOT NULL,
+  field_name int NOT NULL,
+  is_verified int DEFAULT NULL,
+  lastupd_stamp datetime DEFAULT NULL,
+  lastupd_user int DEFAULT NULL,
+  creator_stamp datetime DEFAULT NULL,
+  creator_user int DEFAULT NULL
+)
