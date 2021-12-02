1ff3d4896e69ed9308b548ea72196b5476767374
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-12-03 01:47:27 +0530

    [Vidyasagar]: Added lab Table

--- LMSdata.sql
+++ LMSdata.sql
@@ -211,3 +211,13 @@ CREATE TABLE tech_type (
   creator_stamp datetime DEFAULT NULL,
   creator_user int DEFAULT NULL
 )
+-------------------------------------------------------------------------18) lab---------------------------------------
+CREATE TABLE lab(
+  id int IDENTITY PRIMARY KEY NOT NULL,
+  name varchar(50) DEFAULT NULL,
+  location varchar(50) DEFAULT NULL,
+  address  varchar(255) DEFAULT NULL,
+  status int DEFAULT 1,
+  creator_stamp datetime DEFAULT NULL,
+  creator_user int DEFAULT NULL,
+)
