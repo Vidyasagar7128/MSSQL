fd5cb719250a853304e3b22630a4cde65314155b
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-12-03 00:22:01 +0530

    [Vidyasagar]: Added Candidate_Bank_Details Table

--- LMSdata.sql
+++ LMSdata.sql
@@ -67,3 +67,19 @@ CREATE TABLE candidates_personal_det_check (
   creator_stamp datetime DEFAULT NULL,
   creator_user int DEFAULT NULL
 )
+-------------------------------------------------------------------------4)candidate_bank_det---------------------------------------
+CREATE TABLE candidate_bank_det(
+  id int IDENTITY PRIMARY KEY NOT NULL,
+  name varchar(100) NOT NULL,
+  account_num int NOT NULL,
+  is_account_num_verified int DEFAULT 0,
+  ifsc_code varchar(20) NOT NULL,
+  is_ifsc_code_verified int DEFAULT 0,
+  pan_number varchar(10) DEFAULT NULL,
+  is_pan_number_verified int DEFAULT 0,
+  addhaar_num int NOT NULL,
+  is_addhaar_num_verified int DEFAULT 0,
+  candidate_id int NOT NULL FOREIGN KEY REFERENCES fellowship_candidates(id),
+  creator_stamp datetime DEFAULT NULL,
+  creator_user int DEFAULT NULL,
+)
