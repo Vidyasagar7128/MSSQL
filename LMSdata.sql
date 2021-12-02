02a765576f4bc503226f871be894715c47b2eff6
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-12-03 00:19:30 +0530

    [Vidyasagar]: Added Candidate_personal_detail_check Table

--- LMSdata.sql
+++ LMSdata.sql
@@ -56,3 +56,14 @@ CREATE TABLE fellowship_candidates (
   creator_stamp datetime DEFAULT NULL,
   creator_user int DEFAULT NULL,
 )
+-------------------------------------------------------------------------3) candidates_personal_det_check---------------------------------------
+CREATE TABLE candidates_personal_det_check (
+  id int IDENTITY PRIMARY KEY NOT NULL,
+  candidate_id  int NOT NULL,
+  field_name int NOT NULL,
+  is_verified int DEFAULT NULL,
+  lastupd_stamp datetime DEFAULT NULL,
+  lastupd_user int DEFAULT NULL,
+  creator_stamp datetime DEFAULT NULL,
+  creator_user int DEFAULT NULL
+)
