55c1b5d75a2ca4a5ec37fbb6a211cba33097e8ce
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-12-03 08:05:16 +0530

    [Vidyasagar]: Added app_parameters Table

--- LMSdata.sql
+++ LMSdata.sql
@@ -167,6 +167,19 @@ CREATE TABLE maker_program(
   creator_user int DEFAULT NULL,
 )
 -------------------------------------------------------------------------12) app_parameters---------------------------------------
+CREATE TABLE app_parameters (
+  id int NOT NULL,
+  key_type varchar(20) NOT NULL,
+  key_value varchar(20) NOT NULL,
+  key_text varchar(80) DEFAULT NULL,
+  cur_status char(1) DEFAULT NULL,
+  lastupd_user int DEFAULT NULL,
+  lastupd_stamp datetime DEFAULT NULL,
+  creator_stamp datetime DEFAULT NULL,
+  creator_user int DEFAULT NULL,
+  seq_num int DEFAULT NULL,
+  PRIMARY KEY (key_type,  key_value)
+)
 -------------------------------------------------------------------------13) mentor---------------------------------------
 CREATE TABLE mentor(
   id int NOT NULL IDENTITY PRIMARY KEY,
